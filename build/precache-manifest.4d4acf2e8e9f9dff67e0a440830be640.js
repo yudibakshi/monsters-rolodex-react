ce6216299977de2ad0420bcf46abb671424594b7
Yudi Bakshi <yudibakshi@outlook.com>
2020-10-01 17:24:21 +0530

    adding files for github pages

---
+++ build/precache-manifest.4d4acf2e8e9f9dff67e0a440830be640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0caa03c667e2a501c77e34b8571bdf3",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "2fc2b51ad8a384806c67",
+    "url": "/monsters-rolodex-react/static/css/main.087f6544.chunk.css"
+  },
+  {
+    "revision": "bb47d9729c6421a471e0",
+    "url": "/monsters-rolodex-react/static/js/2.593bfafb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.593bfafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc2b51ad8a384806c67",
+    "url": "/monsters-rolodex-react/static/js/main.c675edb5.chunk.js"
+  },
+  {
+    "revision": "e2967b5039c635a511d3",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.b78413c9.js"
+  }
+]);
