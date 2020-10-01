07f5aa5ae41c9ad696f5760b593955f7f1d21fdd
Yudi Bakshi <yudibakshi@outlook.com>
2020-10-01 21:14:08 +0530

    Updates

---
+++ precache-manifest.c108c90dc68412e82afd2e8a6c791910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0cf5607e94db7101dff34d2d109f0e",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "4376a446b239dcfa613d",
+    "url": "/monsters-rolodex-react/static/css/main.087f6544.chunk.css"
+  },
+  {
+    "revision": "bb47d9729c6421a471e0",
+    "url": "/monsters-rolodex-react/static/js/2.593bfafb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.593bfafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4376a446b239dcfa613d",
+    "url": "/monsters-rolodex-react/static/js/main.0646148e.chunk.js"
+  },
+  {
+    "revision": "e2967b5039c635a511d3",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.b78413c9.js"
+  }
+]);
